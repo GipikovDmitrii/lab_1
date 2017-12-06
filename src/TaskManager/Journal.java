62bf5ab58634db223ff4afea548dc78100f88a9b
AcaD26 <dima.gipikov@yandex.ru>
2017-12-06 19:08:12 +0400

    Add files via upload

--- src/TaskManager/Journal.java
+++ src/TaskManager/Journal.java
@@ -4,6 +4,8 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.io.*;
 import java.util.*;
 
@@ -12,23 +14,25 @@ import java.util.*;
  * @author Дмитрий
  */
 
+@XmlRootElement(name = "journal")
 public class Journal {
-    private List<Task> taskList;
+    @XmlElement
+    private List<Task> task;
 
     Journal() {
-        this.taskList = new ArrayList<>();
+        this.task = new ArrayList<>();
     }
 
     public void add(Task task) {
-        this.taskList.add(task);
+        this.task.add(task);
     }
 
     public void delete(Task task) {
-        this.taskList.remove(task);
+        this.task.remove(task);
     }
 
     public List<Task> getTaskList() {
-        return taskList;
+        return task;
     }
 
     public Task getTask(int index) {
@@ -37,48 +41,46 @@ public class Journal {
     }
 
     public int getSize() {
-        int size = this.taskList.size();
+        int size = this.task.size();
         return size;
     }
 
     public void objectToXml(Journal journal, OutputStream os) throws IOException {
-        for (int i = 0; i < journal.getSize(); i++) {
-           Task task = journal.getTask(i);
-            try {
-                JAXBContext context = JAXBContext.newInstance(Task.class);
-                Marshaller marshaller = context.createMarshaller();
-                marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-                marshaller.marshal(task, os);
-            } catch (JAXBException e) {
-                e.printStackTrace();
-            }
-            os.flush();
-        }
-        os.close();
-    }
-
-    public void xmlToObject(InputStream is) {
-
         try {
-            JAXBContext context = JAXBContext.newInstance(Task.class);
-            Unmarshaller unmarshaller = context.createUnmarshaller();
-            unmarshaller.unmarshal(is);
+            JAXBContext context = JAXBContext.newInstance(Journal.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            marshaller.marshal(journal, os);
+            os.flush();
+            os.close();
         }
         catch (JAXBException e) {
             e.printStackTrace();
         }
     }
 
+    public Journal xmlToObject(String file) {
+        try {
+            JAXBContext context = JAXBContext.newInstance(Journal.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            return (Journal) unmarshaller.unmarshal(new File(file));
+        }
+        catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     public static void main(String[] args) throws IOException {
         FileOutputStream os = new FileOutputStream("journal.xml");
-        FileInputStream is = new FileInputStream("journal.xml");
         String file = "journal.xml";
-        File file1 = new File("journal.xml");
 
         Task task = new Task(1, "1", "1", 1, "1");
         Task task1 = new Task(2, "2", "2", 2, "2");
         Task task2 = new Task(3, "3", "3", 3, "3");
         Journal journal = new Journal();
+        Journal journal1 = new Journal();
+        Journal journal2 = new Journal();
         journal.add(task);
         journal.add(task1);
         journal.add(task2);
@@ -87,6 +89,9 @@ public class Journal {
         System.out.println(journal.getTask(0));
         System.out.println(journal.getTaskList());
         System.out.println(journal.getSize());
-        //journal.xmlToObject(is);
+        journal2.xmlToObject(file);
+        journal2.getSize();
+        //journal2.objectToXml(journal2, os);
+        System.out.println();journal.getTaskList();
     }
 }
